bab00fdb1a3ae3b874eadab626848a4ebd408b22
rahmed712 <ahmedrahat712@gmail.com>
2024-03-27 09:28:44 -0400

    header and footer

---
+++ Tick-It/src/components/footer.jsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faSquareXTwitter, faInstagram, faGithub, faLinkedin, faSnapchat } from '@fortawesome/free-brands-svg-icons'
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-company">
+        <div className="footer-company-title">Company</div>
+        <div className="footer-company-links">
+          <Link to='#' className="footer-link">About</Link>
+          <Link to='#' className="footer-link">Careers</Link>
+          <Link to='#' className="footer-link">Terms</Link>
+          <Link to='#' className="footer-link">Privacy</Link>
+        </div>
+      </div>
+      <div className="footer-support">
+        <div className="footer-support-title">Support</div>
+        <div className="footer-support-links">
+          <Link to='#' className="footer-link">FAQs</Link>
+          <Link to='#' className="footer-link">Trust and Safety</Link>
+          <Link to='#' className="footer-link">Cookie Preferences</Link>
+          <Link to='#' className="footer-link">Report Issue</Link>
+        </div>
+      </div>
+      <div className="footer-contact">
+        <div className="footer-copyright">©2024</div>
+          <div className="footer-appname">Tick-It</div>
+          <div className="footer-social-icons">
+            <FontAwesomeIcon icon={faFacebook} />
+            <FontAwesomeIcon icon={faSquareXTwitter} />
+            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faGithub} />
+            <FontAwesomeIcon icon={faLinkedin} />
+            <FontAwesomeIcon icon={faSnapchat} />
+          </div>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
